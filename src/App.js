f9770323c0fab7c27c8921b8e3cbc687f2dea6bf
AvoKoppas <avo.okas@live.com>
2021-05-31 20:15:20 +0300

    Still working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useCallback, useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [responseData, setResponseData] = useState(['Here comes info']);
+    const options = {
+        method: 'GET',
+        url: 'https://covid-193.p.rapidapi.com/statistics',
+        params: {country: 'estonia'},
+        headers: {
+            'x-rapidapi-key': '457a49cbd2msh587809b267ee184p1965ffjsn4b06828b27cb',
+            'x-rapidapi-host': 'covid-193.p.rapidapi.com'
+        },
+    };
+    const fetchData = useCallback(() => {
+        axios(options)
+
+            .then((response) => {
+                console.log(response)
+                setResponseData(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <button type='button' onClick={fetchData}> STATISTICS FOR ESTONIA
+                </button>
+
+                {JSON.stringify(responseData.data)}
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
